ee74c7ad8478a8dc331ed8fe18beb804bb19c04f
mustafa muse <muse3mm@gmail.com>
2022-05-16 15:08:52 +0200

    change NotFound.

---
+++ app/src/components/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="notfound">
+		<h1>Wooooops!</h1>
+		<p>Page Not Found<</p>
+		<router-link class="notfound__button" to="/">
+			<button>Click here and we'll guide you back to home!</button>
+		</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style>
+.notfound {
+	text-align: center;
+	margin: var(--large-spacing);
+}
+.notfound__button {
+	display: block;
+	max-width: 500px;
+	margin: var(--medium-spacing) auto;
+	background: rgb(243, 243, 243);
+	border: 2px solid rgb(49, 49, 49);
+	padding: 1em;
+}
+</style>
